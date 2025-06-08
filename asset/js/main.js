634740590e2297b27dc3a00ce69aebf3245200be
aProject2021 <aliciaproyectos2021@gmail.com>
2025-06-08 18:04:23 -0500

    animaciones y tipografia

--- asset/js/main.js
+++ asset/js/main.js
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function () {
     navLinks.classList.remove('open');
     overlay.classList.remove('active');
   });
- 
+
   document.querySelectorAll('.service-item').forEach(item => {
     item.addEventListener('click', () => {
       document.querySelectorAll('.service-item').forEach(el => el.classList.remove('active'));
       item.classList.add('active');
-  
+
       const target = item.getAttribute('data-target');
       document.querySelectorAll('.gallery .imagen').forEach(img => {
         img.classList.remove('active');
@@ -31,7 +31,24 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
-  
-  
+
+   const rawText = "El arte de construir marcas\nque hacen la diferencia";
+    const h1 = document.getElementById("typed");
+    let i = 0;
+
+    function type() {
+      if (i <= rawText.length) {
+        // Tomamos solo la parte escrita hasta ahora
+        const partial = rawText.substring(0, i);
+        // Reemplazamos los \n por <br> justo antes de mostrarlo
+        h1.innerHTML = partial.replace(/\n/g, "<br>");
+        i++;
+        setTimeout(type, 80);
+      } else {
+        h1.style.borderRight = "none";
+      }
+    }
+
+    type();
 
 });
