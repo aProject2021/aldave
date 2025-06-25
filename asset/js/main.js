cc3ce4ad17f4ab887ad69d31223d34681c9fb1cf
aProject2021 <aliciaproyectos2021@gmail.com>
2025-06-25 11:16:27 -0500

    Audio

--- asset/js/main.js
+++ asset/js/main.js
@@ -39,17 +39,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const h1 = document.getElementById("typed");
   let i = 0;
+  const typeSound = document.getElementById("typeSound");
+  let audioAllowed = false;
+
+  document.addEventListener("click", () => {
+    audioAllowed = true;
+  }, { once: true });
 
   function type() {
+
+
+
     if (i <= rawText.length) {
       // Tomamos solo la parte escrita hasta ahora
       const partial = rawText.substring(0, i);
       // Reemplazamos los \n por <br> justo antes de mostrarlo
       h1.innerHTML = partial.replace(/\n/g, "<br>");
+      if (audioAllowed) {
+        // Reproducir el audio una sola vez al iniciar
+        typeSound.currentTime = 0;
+        audioAllowed = false
+        typeSound.play().catch(err => {
+          console.log("Audio bloqueado por falta de interacción");
+        });
+      }
       i++;
       setTimeout(type, 80);
     } else {
       h1.style.borderRight = "none";
+      // Detener el audio cuando termina de escribir
+      typeSound.pause();
+      typeSound.currentTime = 0;
     }
   }
 
